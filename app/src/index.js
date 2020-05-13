4ddd8ec39462df17db48171f3878591081eb538a
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-05-13 15:09:01 -0400

    initial commit

---
+++ app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//redux imports
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import { reducer } from './store/reducers/reducer'
+import thunk from 'redux-thunk';
+
+export const store = createStore(reducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>, 
+    rootElement);
